dfee4f18a0fa78e4894f3566a28b1732f94c389e
austin-j-taylor <austin@teamtaylor.org>
2016-09-11 14:10:11 -0400

    Migrated songs from Melody to Melody_2
    Added metronome functionality to Melody_2

--- Arduino/Melody_2/Melody_2.ino
+++ Arduino/Melody_2/Melody_2.ino
@@ -2,9 +2,7 @@
 #include <math.h>
 
 /* TODO
-  metronome
-  fix playing songs after another has played
-  migrate songs from Melody to here
+ *  fix songs playing without reseting
 */
 //** ReacXion Source Code **//
 //** www.jeremyblum.com **//
@@ -17,6 +15,7 @@
 // used to describe conversion from Hz to microseconds for Arduino
 #define MICROC 1000000.0 / 2 / SPEEDCONST
 // used in calculation of frequencies
+// Frequency 'n' semitones away from Base Frequency = Base Frequency * ((2^(1/12))^n)
 #define twototheonetwelfth pow(2, (1 / 12.0))
 
 /* Timer reload value, globally available */
@@ -34,7 +33,7 @@ volatile unsigned int countf[] = { 0, 0 };
 volatile unsigned int countd[] = { 0, 0 };
 
 //bool volatile playNext[] = { true, true };
-bool playingSong[] = { false, false };
+volatile bool playingSong[] = { false, false };
 
 // holds the  time to play a note in units of 64-microseconds
 unsigned int duration[] = { 0, 0 };
@@ -45,15 +44,19 @@ volatile unsigned char motifIndex[] = { 0, 0 };
 // holds the index of the current Note of a Motif to play
 volatile unsigned char noteIndex[] = { 0, 0 };
 
+// indicates if the metronome is currently playing
+volatile bool playingMetronome = false;
+
 /* Frequency Output Pins */
 const char pin[] = { 9, 10 };
 // length of songs (number of motifs)
 char length[] = { 0, 0 };
 
-// Frequency 'n' semitones away from Base Frequency = Base Frequency * ((2^(1/12))^n)
 double key = 220.0;
 double beatInMilliseconds;
 unsigned int noteIsOneBeat;
+unsigned char beatsPerMeasure;
+unsigned char measuresToPlay;
 // durations: 64 = whole note, 32 = half note, 16 = quarter note, 8 = eighth note, 4 = sixteenth note
 
 /* CLASSES USED IN FORMATION OF SONGS */
@@ -114,6 +117,26 @@ class Songbook {
     static Note n_Clocktown7[7];
     static Note n_Clocktown8[11];
 
+    static Note n_ZeldasLullaby1[9];
+    static Note n_ZeldasLullaby2[9];
+    static Note n_ZeldasLullaby3[6];
+    static Note n_ZeldasLullaby4[5];
+    static Note n_ZeldasLullaby5[7];
+    static Note n_ZeldasLullaby6[4];
+    static Note n_BM1[6];
+    static Note n_BM2[6];
+    static Note n_BM3[6];
+    static Note n_BM4[6];
+    static Note n_BM5[7];
+    static Note n_BM6[4];
+    static Note n_BM7[6];
+    static Note n_BM8[8];
+    static Note n_BM9[7];
+    static Note n_BM10[8];
+    static Note n_BM11[8];
+    static Note n_BM12[10];
+    static Note n_BM13[5];
+
     static Note n_DragonsREST1[4];
     static Note n_DragonsB1[10];
     static Note n_DragonsB2[9];
@@ -151,6 +174,26 @@ class Songbook {
     static Motif* m_Clocktown7;
     static Motif* m_Clocktown8;
 
+    static Motif* m_ZeldasLullaby1;
+    static Motif* m_ZeldasLullaby2;
+    static Motif* m_ZeldasLullaby3;
+    static Motif* m_ZeldasLullaby4;
+    static Motif* m_ZeldasLullaby5;
+    static Motif* m_ZeldasLullaby6;
+    static Motif* m_BM1;
+    static Motif* m_BM2;
+    static Motif* m_BM3;
+    static Motif* m_BM4;
+    static Motif* m_BM5;
+    static Motif* m_BM6;
+    static Motif* m_BM7;
+    static Motif* m_BM8;
+    static Motif* m_BM9;
+    static Motif* m_BM10;
+    static Motif* m_BM11;
+    static Motif* m_BM12;
+    static Motif* m_BM13;
+
     static Motif* m_DragonsREST1;
     static Motif* m_DragonsB1;
     static Motif* m_DragonsB2;
@@ -179,6 +222,10 @@ class Songbook {
     static Motif* s_ClocktownBass[11];
     static Motif* s_Clocktown[35];
 
+    static Motif* s_ZeldasLullaby[8];
+
+    static Motif* s_BanneredMare[16];
+
     static Motif* s_DragonsBass[37];
     static Motif* s_Dragons[24];
 
@@ -187,6 +234,10 @@ class Songbook {
   public:
     static Song ClocktownBass;
     static Song Clocktown;
+
+    static Song ZeldasLullaby;
+
+    static Song BanneredMare;
 
     static Song DragonsBass;
     static Song Dragons;
@@ -231,6 +282,67 @@ Note Songbook::n_Clocktown7[7] = {
 Note Songbook::n_Clocktown8[11] = {
   Note(126, 96), Note(126, 96), Note(126, 96), Note(126, 96),
   Note(126, 96), Note(126, 96), Note(126, 32), Note(7 + 12, 16), Note(19 + 12, 32), Note(7 + 12, 16), Note(19 + 12, 96)
+};
+
+Note Songbook::n_ZeldasLullaby1[9] = {
+  Note(4, 32), Note(7, 16), Note(2, 32), Note(0, 8), Note(2, 8), Note(4, 32), Note(7, 16), Note(2, 32), Note(126, 16) // x9
+};
+Note Songbook::n_ZeldasLullaby2[9] = {
+  Note(4, 32), Note(7, 16), Note(14, 32), Note(12, 16), Note(7, 32), Note(5, 8), Note(4, 8), Note(2, 32), Note(126, 16) // x9
+};
+Note Songbook::n_ZeldasLullaby3[6] = {
+  Note(4, 32), Note(7, 16), Note(14, 32), Note(12, 16), Note(19, 64), Note(126, 32) // x6
+};
+Note Songbook::n_ZeldasLullaby4[5] = {
+  Note(19, 32), Note(17, 8), Note(16, 8), Note(17, 8), Note(16, 8) // x5 //, Note(26, 32)
+};
+Note Songbook::n_ZeldasLullaby5[7] = {
+  Note(14, 32), // x1
+  Note(17, 32), Note(16, 8), Note(14, 8), Note(16, 8), Note(14, 8), Note(9, 32) // x6
+};
+Note Songbook::n_ZeldasLullaby6[4] = {
+  Note(14, 16), Note(17, 16), Note(24, 64), Note(126, 32) // x4
+};
+
+Note Songbook::n_BM1[6] = {
+  Note(15, 48), Note(12, 16), Note(14, 32), Note(15, 48), Note(17, 16), Note(15, 32) // x6
+};
+Note Songbook::n_BM2[6] = {
+  Note(14, 48), Note(10, 16), Note(12, 32), Note(14, 48), Note(15, 16), Note(14, 32) // x6
+};
+Note Songbook::n_BM3[6] = {
+  Note(12, 48), Note(8, 16), Note(10, 32), Note(12, 48), Note(14, 16), Note(12, 32) // x6
+};
+Note Songbook::n_BM4[6] = {
+  Note(11, 48), Note(12, 16), Note(9, 32), Note(11, 48), Note(126, 16), Note(7, 32)// x6
+};
+Note Songbook::n_BM5[7] = {
+  Note(12, 48), Note(14, 16), Note(12, 16),  Note(126, 16), Note(12, 48), Note(14, 16), Note(12, 32) // x7
+};
+Note Songbook::n_BM6[4] = {
+  Note(11, 64), Note(14, 32), Note(12, 64), Note(126, 32) // x4
+};
+Note Songbook::n_BM7[6] = {
+  Note(19, 48), Note(15, 16), Note(17, 32), Note(19, 48), Note(20, 16), Note(19, 32) // x6
+};
+Note Songbook::n_BM8[8] = {
+  Note(17, 48), Note(14, 16), Note(15, 32), Note(17, 48), Note(19, 16), Note(17, 16), Note(15, 8), Note(99, 8) // x8
+};
+// + n_BM1
+Note Songbook::n_BM9[7] = {
+  Note(14, 32 / 3), Note(15, 32 / 3), Note(14, 32 / 3 + 48 + 8), Note(126, 8), Note(14, 64), Note(10, 24), Note(126, 8) // x7
+};
+Note Songbook::n_BM10[8] = {
+  Note(19, 64), Note(15, 16), Note(17, 16), Note(19, 32), Note(20, 16), Note(19, 16), Note(17, 16), Note(15, 16) // x8
+};
+Note Songbook::n_BM11[8] = {
+  Note(17, 64), Note(14, 16), Note(15, 16), Note(17, 32), Note(19, 16), Note(17, 16), Note(15, 16), Note(14, 16) // x8
+};
+Note Songbook::n_BM12[10] = {
+  Note(15, 32), Note(12, 16), Note(14, 16), Note(15, 24), Note(126, 8), Note(15, 32), Note(17, 16), Note(15, 16), Note(14, 16), Note(12, 16) // x10
+};
+Note Songbook::n_BM13[5] = {
+  Note(14, 32), Note(10, 32), Note(14, 32), Note(12, 64), Note(126, 32) // x5
 };
 
 Note Songbook::n_DragonsREST1[4] = {
@@ -327,6 +439,27 @@ Motif* Songbook::m_Clocktown6 = new Motif(n_Clocktown6, 8);
 Motif* Songbook::m_Clocktown7 = new Motif(n_Clocktown7, 7);
 Motif* Songbook::m_Clocktown8 = new Motif(n_Clocktown8, 11);
 
+Motif* Songbook::m_ZeldasLullaby1 = new Motif(n_ZeldasLullaby1, 9);
+Motif* Songbook::m_ZeldasLullaby2 = new Motif(n_ZeldasLullaby2, 9);
+Motif* Songbook::m_ZeldasLullaby3 = new Motif(n_ZeldasLullaby3, 6);
+Motif* Songbook::m_ZeldasLullaby4 = new Motif(n_ZeldasLullaby4, 5);
+Motif* Songbook::m_ZeldasLullaby5 = new Motif(n_ZeldasLullaby5, 7);
+Motif* Songbook::m_ZeldasLullaby6 = new Motif(n_ZeldasLullaby6, 4);
+
+Motif* Songbook::m_BM1 = new Motif(n_BM1, 6);
+Motif* Songbook::m_BM2 = new Motif(n_BM2, 6);
+Motif* Songbook::m_BM3 = new Motif(n_BM3, 6);
+Motif* Songbook::m_BM4 = new Motif(n_BM4, 6);
+Motif* Songbook::m_BM5 = new Motif(n_BM5, 7);
+Motif* Songbook::m_BM6 = new Motif(n_BM6, 4);
+Motif* Songbook::m_BM7 = new Motif(n_BM7, 6);
+Motif* Songbook::m_BM8 = new Motif(n_BM8, 8);
+Motif* Songbook::m_BM9 = new Motif(n_BM9, 7);
+Motif* Songbook::m_BM10 = new Motif(n_BM10, 8);
+Motif* Songbook::m_BM11 = new Motif(n_BM11, 8);
+Motif* Songbook::m_BM12 = new Motif(n_BM12, 10);
+Motif* Songbook::m_BM13 = new Motif(n_BM13, 5);
+
 Motif* Songbook::m_DragonsREST1 = new Motif(n_DragonsREST1, 4);
 Motif* Songbook::m_DragonsB1 = new Motif(n_DragonsB1, 10);
 Motif* Songbook::m_DragonsB2 = new Motif(n_DragonsB2, 9);
@@ -385,6 +518,21 @@ Motif* Songbook::s_Clocktown[35] = {
   m_Clocktown5, m_Clocktown6, m_Clocktown5, m_Clocktown7
 };
 
+Motif* Songbook::s_ZeldasLullaby[8] = {
+  m_ZeldasLullaby1, m_ZeldasLullaby2,
+  m_ZeldasLullaby1, m_ZeldasLullaby3,
+
+  m_ZeldasLullaby4, m_ZeldasLullaby5,
+  m_ZeldasLullaby4, m_ZeldasLullaby6
+};
+
+Motif* Songbook::s_BanneredMare[16] = {
+  m_BM1, m_BM2, m_BM3, m_BM4,
+  m_BM1, m_BM2, m_BM5, m_BM6,
+  m_BM7, m_BM8, m_BM1, m_BM9,
+  m_BM10, m_BM11, m_BM12, m_BM13
+};
+
 Motif* Songbook::s_DragonsBass[37] = {
   m_DragonsREST1,
 
@@ -432,6 +580,10 @@ Motif* Songbook::s_CustomBass[12] = {
 Song Songbook::ClocktownBass = Song(s_ClocktownBass, 11, 90, 8);
 Song Songbook::Clocktown = Song(s_Clocktown, 35, 90, 8);
 
+Song Songbook::BanneredMare = Song(s_BanneredMare, 16, 90, 8);
+
+Song Songbook::ZeldasLullaby = Song(s_ZeldasLullaby, 8, 60, 4);
+
 Song Songbook::DragonsBass = Song(s_DragonsBass, 37, 105, 4);
 Song Songbook::Dragons = Song(s_Dragons, 24, 105, 4);
 
@@ -442,7 +594,8 @@ void setKey(char);
 void setTempo(char);
 double getFreq(char);
 char getMicros(double);
-void playSong( Song, char);
+void setMeter(Song, unsigned char, unsigned char);
+void playSong(Song, char);
 
 Song* volatile songChannel;
 Note* volatile currentNote;
@@ -495,19 +648,21 @@ double getFreq(char distance) {
 char getMicros(double freq) {
   return (char) (MICROC / freq + .5);
 }
-// plays a song on the specified channel
+// sets the meter based on the given song
+void setMeter(Song song, unsigned char bPM = 0, unsigned char mTP = 0) {
+  setNoteIsOneBeat(song.defaultNoteIsOneBeat);
+  setTempo(song.defaultTempo);
+  beatsPerMeasure = bPM;
+  measuresToPlay = mTP;
+}
+// activates a song on the specified channel
 void playSong(Song song, char channel) {
-  // only assigns once
-  if (channel == 0) {
-    setNoteIsOneBeat(song.defaultNoteIsOneBeat);
-    setTempo(song.defaultTempo);
-  }
   songChannel[channel] = song;
   length[channel] = song.getlength();
   playingSong[channel] = true;
 }
 void setup() {
-  Serial.begin(9600);
+  //  Serial.begin(9600);
   setKey('D'); // C D E F G A B
   /* TIMER STUFF */
   /* First disable the timer overflow interrupt*/
@@ -549,61 +704,14 @@ void setup() {
   pinMode(5,    INPUT);
   pinMode(8,    INPUT);
 }
-
 /* Installs the interrupt Service Routine (ISR) for Timer2, executed every SPEEDCONST microseconds */
 ISR(TIMER2_OVF_vect) {
   /* Reload the timer */
   TCNT2 = tcnt2;
-  // increment counters
-  countd[0]++;
-  countd[1]++;
-
-  /* Note the lack of a for-loop for iterating through each channel. The Arduino REALLY doesn't like having a for loop in an Interrupt, so I had to copy-paste stuff instead. Sorry. */
-  /* Also, for SOME reason, the channels have to execute in 1-followed-by-0 order, rather than 0-to-1. Otherwise, channel 0 skips the first note. And no, in 1-to-0, channel 1 doesn't skip its first note. ¯\_(ツ)_/¯  */
+  /* Note the lack of a for-loop for iterating through each channel. The notes sound slightly more grainy if the commands are executed through a for-loop. */
 
   // goes through each channel, sees if it's playing a song
-  if (playingSong[1]) {
-    /* PLAY FREQUENCY 1 */
-    if (freq[1] != 126) {
-      countf[1]++;
-      if (freq[1] == countf[1]) {
-        digitalWrite(pin[1], toggle[1] == 0 ? HIGH : LOW);
-        toggle[1] = ~toggle[1];
-        countf[1] = 0;
-      }
-    } else {
-      //        digitalWrite(pin[1], LOW);
-      countf[1] = 0;
-    }
-
-    // if the current note has played as long as it should
-    if (countd[1] == duration[1]) {
-      countd[1] = 0;
-      countf[1] = 0;
-
-      /* BASICALLY A TRANS-INTERRUPT FOR LOOP... OH BOY */
-      // figures out what note to play next or if the song should end
-      if (noteIndex[1] < songChannel[1].motifs[motifIndex[1]]->length) { // same motif as before, next note
-      } else { // next motif
-        motifIndex[1]++;
-        noteIndex[1] = 0;
-      }
-      if (motifIndex[1] == length[1]) { // end of song
-        freq[1] =  126;
-        playingSong[1] = false;
-        noteIndex[1] = 0;
-        motifIndex[1] = 0;
-        duration[1] = 0;
-      } else {
-        currentNote = &songChannel[1].motifs[motifIndex[1]]->notes[noteIndex[1]];
-        duration[1] = (currentNote->duration) * beatInMilliseconds / noteIsOneBeat * 1000 / SPEEDCONST;
-        freq[1] = (currentNote->distance == '~' /* ascii code 126 */ ) ? 126 : getMicros(getFreq(currentNote->distance)); // 126? rest if true, normal frequency if not
-        noteIndex[1]++;
-      }
-    }
-  }
-  if (playingSong[0]) {
-    /* PLAY FREQUENCY 0 */
+  if (playingMetronome) { // metronome is playing
     if (freq[0] != 126) {
       countf[0]++;
       if (freq[0] == countf[0]) {
@@ -612,31 +720,119 @@ ISR(TIMER2_OVF_vect) {
         countf[0] = 0;
       }
     } else {
-      //        digitalWrite(pin[0], LOW);
       countf[0] = 0;
     }
-
-    /* CHANNEL 0 */
     if (countd[0] == duration[0]) {
       countd[0] = 0;
       countf[0] = 0;
-      //      Serial.println(songChannel[0].getlength());
-      if (noteIndex[0] < songChannel[0].motifs[motifIndex[0]]->length) { // same motif as before, next note
-      } else { // next motif
-        motifIndex[0]++;
-        noteIndex[0] = 0;
-      }
-      if (motifIndex[0] == length[0]) { // end of song
+
+      if (freq[0] != 126) { // before was beep, now rest
+        duration[0] = 14 * beatInMilliseconds / noteIsOneBeat * 1000 / SPEEDCONST;
         freq[0] = 126;
-        playingSong[0] = false;
-        noteIndex[0] = 0;
-        motifIndex[0] = 0;
-        duration[0] = 0;
+      } else { // before was rest, now beep
+        if (noteIndex[0] == beatsPerMeasure && motifIndex[0] == measuresToPlay) { // end of metronome
+          freq[0] = 126;
+          playingMetronome = false;
+          noteIndex[0] = 0;
+          motifIndex[0] = 0;
+          duration[0] = 0;
+        } else { // not end of metronome
+          if (noteIndex[0] == 0 || noteIndex[0] == beatsPerMeasure) { // end of measure, proceed to next
+            freq[0] = getMicros(getFreq(24));
+            noteIndex[0] = 0;
+            motifIndex[0]++;
+          } else { // not start of measure
+            freq[0] = getMicros(getFreq(12));
+          }
+          duration[0] = 2 * beatInMilliseconds / noteIsOneBeat * 1000 / SPEEDCONST;
+          noteIndex[0]++;
+        }
+      }
+    } else {
+      countd[0]++;
+    }
+  } else { // metronome is no longer playing/not yet playing
+    if (playingSong[0]) {
+      /* PLAY FREQUENCY 0 */
+      if (freq[0] != 126) {
+        countf[0]++;
+        if (freq[0] == countf[0]) {
+          digitalWrite(pin[0], toggle[0] == 0 ? HIGH : LOW);
+          toggle[0] = ~toggle[0];
+          countf[0] = 0;
+        }
       } else {
-        currentNote = &songChannel[0].motifs[motifIndex[0]]->notes[noteIndex[0]];
-        duration[0] = (currentNote->duration) * beatInMilliseconds / noteIsOneBeat * 1000 / SPEEDCONST;
-        freq[0] = (currentNote->distance ==  '~') ? 126 : getMicros(getFreq(currentNote->distance)); // 126? rest if true, normal frequency if not
-        noteIndex[0]++;
+        //        digitalWrite(pin[0], LOW);
+        countf[0] = 0;
+      }
+
+      /* CHANNEL 0 */
+      if (countd[0] == duration[0]) {
+        countd[0] = 0;
+        countf[0] = 0;
+        if (noteIndex[0] < songChannel[0].motifs[motifIndex[0]]->length) { // same motif as before, next note
+        } else { // next motif
+          motifIndex[0]++;
+          noteIndex[0] = 0;
+        }
+        if (motifIndex[0] == length[0]) { // end of song
+          freq[0] = 126;
+          playingSong[0] = false;
+          playingSong[1] = false;
+          noteIndex[0] = 0;
+          motifIndex[0] = 0;
+          duration[0] = 0;
+        } else {
+          currentNote = &songChannel[0].motifs[motifIndex[0]]->notes[noteIndex[0]];
+          duration[0] = (currentNote->duration) * beatInMilliseconds / noteIsOneBeat * 1000 / SPEEDCONST;
+          freq[0] = (currentNote->distance ==  '~') ? 126 : getMicros(getFreq(currentNote->distance)); // 126? rest if true, normal frequency if not
+          noteIndex[0]++;
+        }
+      } else {
+        countd[0]++;
+      }
+    }
+    if (playingSong[1]) {
+      /* PLAY FREQUENCY 1 */
+      if (freq[1] != 126) {
+        countf[1]++;
+        if (freq[1] == countf[1]) {
+          digitalWrite(pin[1], toggle[1] == 0 ? HIGH : LOW);
+          toggle[1] = ~toggle[1];
+          countf[1] = 0;
+        }
+      } else {
+        //        digitalWrite(pin[1], LOW);
+        countf[1] = 0;
+      }
+
+      // if the current note has played as long as it should
+      if (countd[1] == duration[1]) {
+        countd[1] = 0;
+        countf[1] = 0;
+
+        /* BASICALLY A TRANS-INTERRUPT FOR LOOP... OH BOY */
+        // figures out what note to play next or if the song should end
+        if (noteIndex[1] < songChannel[1].motifs[motifIndex[1]]->length) { // same motif as before, next note
+        } else { // next motif
+          motifIndex[1]++;
+          noteIndex[1] = 0;
+        }
+        if (motifIndex[1] == length[1]) { // end of song, reset values
+          freq[1] =  126;
+          playingSong[1] = false;
+          playingSong[0] = false;
+          noteIndex[1] = 0;
+          motifIndex[1] = 0;
+          duration[1] = 0;
+        } else {
+          currentNote = &songChannel[1].motifs[motifIndex[1]]->notes[noteIndex[1]];
+          duration[1] = (currentNote->duration) * beatInMilliseconds / noteIsOneBeat * 1000 / SPEEDCONST;
+          freq[1] = (currentNote->distance == '~' /* ascii code 126 */ ) ? 126 : getMicros(getFreq(currentNote->distance)); // 126? rest if true, normal frequency if not
+          noteIndex[1]++;
+        }
+      } else {
+        countd[1]++;
       }
     }
   }
@@ -645,8 +841,6 @@ ISR(TIMER2_OVF_vect) {
 void loop() {
   // reading of buttons to decide what song to play
   // comments beside conditionals show what button presses activate that conditional
-  //  Serial.print(playingSong[0]);
-  //  Serial.println(playingSong[1]);
   if (!playingSong[0] && !playingSong[1]) {
     if (digitalRead(4) == HIGH || digitalRead(5) == HIGH || digitalRead(8) == HIGH) {
       delay(100);
@@ -660,22 +854,31 @@ void loop() {
           else {} //YYN
       //          tone(pin[0], 660);
         else //YN_
-          if (state8 == HIGH) {}//YNY
-      //          tone(pin[0], 550);
+          if (state8 == HIGH) { //YNY
+            setMeter(Songbook::BanneredMare);
+            playSong(Songbook::BanneredMare, 0);
+          }
           else {
+            setMeter(Songbook::CustomBass, 4, 2);
+            playingMetronome = true;
             playSong(Songbook::CustomBass, 0);
+            // ignore this. program cannot work with out this statement. Once again, no idea why. It doesn't even print anything out, as the serial monitor isn't set to be receiving anything.
+            Serial.println(playingSong[0]);
           } //YNN
       else  //N__
         if (state5 == HIGH) //NY_
-          if (state8 == HIGH) {}//NYY
-      //          tone(pin[0], 440);
+          if (state8 == HIGH) { //NYY
+            setMeter(Songbook::ZeldasLullaby);
+            playSong(Songbook::ZeldasLullaby, 0);
+          }
           else { //NYN
+            setMeter(Songbook::Dragons);
             playSong(Songbook::Dragons, 0);
             playSong(Songbook::DragonsBass, 1);
           }
         else  //NN_
           if (state8 == HIGH) { //NNY
-            //          tone(pin[0], 110);
+            setMeter(Songbook::Clocktown);
             playSong(Songbook::Clocktown, 0);
             playSong(Songbook::ClocktownBass, 1);
           }
